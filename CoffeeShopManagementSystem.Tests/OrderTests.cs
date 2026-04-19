dfacaa308778ddb429b88960b0d352f6994f2990
øwo <owolsen@gmail.com>
2026-04-19 22:53:38 +0200

    Add placeholder test in OrderTests for xUnit setup

---
+++ CoffeeShopManagementSystem.Tests/OrderTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+using CoffeeShopManagementSystem.Entities;
+
+namespace CoffeeShopManagementSystem.Tests;
+
+public class OrderTests
+{
+    [Fact]
+    public void Placeholder_ShouldRun()
+    {
+        Assert.True(true);
+    }
+}
