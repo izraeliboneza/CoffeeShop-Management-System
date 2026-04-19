26785444767cd3b66710b2e7eeb242e8fab4234b
Kjell-Ivar Pettersen <kip@homegeek.no>
2026-04-19 03:07:06 +0200

    Updated classes in Entities folder  in line with system flow

--- CoffeeShopManagementSystem/Entities/Employee.cs
+++ CoffeeShopManagementSystem/Entities/Employee.cs
@@ -3,11 +3,34 @@ namespace CoffeeShopManagementSystem.Entities
     // Base class for all employees in the system.
     public abstract class Employee
     {
+        private string _id;
+        private string _name;
+
         // Employee ID, for example "B001" or "S001".
-        public string Id { get; protected set; }
+        public string Id
+        {
+            get => _id;
+            protected set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Employee ID cannot be empty.");
+
+                _id = value;
+            }
+        }
 
         // Full name of the employee.
-        public string Name { get; protected set; }
+        public string Name
+        {
+            get => _name;
+            protected set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Name cannot be empty.");
+
+                _name = value;
+            }
+        }
 
         // This runs when a Barista or Supervisor object is created.
         protected Employee(string id, string name)
