4bb569b7e5577cc5f1567a2cc39746f53776107f
Kjell-Ivar Pettersen <kip@homegeek.no>
2026-04-19 02:57:37 +0200

    Updated classes in Entities in line with system flow

--- CoffeeShopManagementSystem/Entities/Supervisor.cs
+++ CoffeeShopManagementSystem/Entities/Supervisor.cs
@@ -1,28 +1,17 @@
-using System.Reflection.Metadata;
-
 namespace CoffeeShopManagementSystem.Entities
 {
-    // Supervisor arver fra Employee.
-    // REpresenterer supervisor ansatt.
+    // Supervisor employee inherits from abstract Employee class.
     public class Supervisor : Employee
     {
-        // returnerer rolle-navnet.
-        public override string Role => "Supervisor";
-
-        // Oppretter en ny supervisor med ID og navn.
-        public Supervisor(string id, string name) : base(id, name) { }
-
-        // Supervisor skal vise kun ID or rolle.
-        public override void DisplayInfo()
+        // Creates a new Supervisor with ID and name.
+        public Supervisor(string id, string name) : base(id, name)
         {
-            Console.WriteLine($"Logged in as: {Role}");
         }
 
-        // Supervisor skal ha tilgang til alle menyene.
-        // new order, order history, sales overviewe, coffee menu, switch UserStringHandle og exit.
-        public override List<int> GetMenuOptions()
+        // Returns the role name shown in the menu and header.
+        public override string GetRoleName()
         {
-            return new List<int>{1, 2, 3, 4, 5, 6};
+            return "Supervisor";
         }
     }
 }
