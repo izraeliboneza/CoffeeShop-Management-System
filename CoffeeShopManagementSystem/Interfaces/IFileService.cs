580b0dce7d142748a8e716e214a6badf484b695c
izraeliboneza <izraeliboneza@gmail.com>
2026-04-21 12:03:11 +0200

    Implement Interfaces (IFileService, IPaymentProcessor)

---
+++ CoffeeShopManagementSystem/Interfaces/IFileService.cs
@@ -0,0 +1,19 @@
+using CoffeeShopManagementSystem.Entities;
+
+namespace CoffeeShopManagementSystem.Interfaces;
+
+
+// This interface defines what a file service must be able to do.
+//JsonFileService implements this interface and handles the actual saving and loading.
+//Because OrderService uses IfileService and not JsonFileService directly,
+//you could replace JSON storage with a database later without having to
+//change anything in OrderService.
+public interface IFileService
+{
+   // Saves completed order to the backing store.
+   public void Save(Order order);
+   
+   //Loads the saved orders from the backing store. 
+   //Return an empty list if there is no orders saved.
+   public List<Order> LoadOrders();
+}
